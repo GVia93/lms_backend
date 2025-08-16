2a17b8322ae14d2c23d5a50869928a2072504ad7
gvia <gritcaev.vv@gmail.com>
2025-08-16 17:08:02 +0300

    fix

--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("lms.urls")),
-    path("api/", include("users.urls")),
-]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    path("api/users/", include(("users.urls", "users"), namespace="users")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import PaymentListAPIView
 
 
+app_name = "users"
+
 urlpatterns = [
     path("payments/", PaymentListAPIView.as_view(), name="payments-list"),
 ]
